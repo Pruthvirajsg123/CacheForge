6fc35a812434b724abf5d7e87f5857400a1646de
Pruthviraj Sg <pruthvirajsg123@gmail.com>
2026-09-04 00:57:23 +0530

    feat: combine hashmap and DLL into LRUStore

--- src/lru/LRUStore.js
+++ src/lru/LRUStore.js
@@ -1,6 +1,37 @@
+const Node = require("./Node");
+const DoublyLinkedList = require("./DoublyLinkedList");
+
 class LRUStore {
   constructor() {
     this.cache = new Map();
+    this.list = new DoublyLinkedList();
+  }
+
+  set(key, value) {
+    const existingNode = this.cache.get(key);
+
+    if (existingNode) {
+      existingNode.value = value;
+      this.list.moveToHead(existingNode);
+      return;
+    }
+
+    const newNode = new Node(key, value);
+
+    this.cache.set(key, newNode);
+    this.list.addToHead(newNode);
+  }
+
+  get(key) {
+    const node = this.cache.get(key);
+
+    if (!node) {
+      return undefined;
+    }
+
+    this.list.moveToHead(node);
+
+    return node.value;
   }
 }
 
